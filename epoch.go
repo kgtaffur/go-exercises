6b5cef52698c1a422b13826691ed6ad09cb4443d
Kevin <86487929+tar83@users.noreply.github.com>
2022-12-26 11:55:57 -0500

    added: epoch program

---
+++ epoch.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	p := fmt.Println
+	current_time := time.Now()
+	secs := current_time.Unix()
+	nanos := current_time.UnixNano()
+
+	p(current_time)
+	millis := nanos / 1000000
+	p(secs)
+	p(millis)
+	p(nanos)
+	p(time.Unix(secs, 0))
+	p(time.Unix(0, nanos))
+}
